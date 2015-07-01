31820aa414e0365e82e4ae9ddc673034b3c6cf23
Ernest Lee <elee@paywhere.com>
2015-07-01 17:15:26 +0800

    update image loading for live reload.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,10 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-//var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-//var mountFolder = function (connect, dir) {
-//  return connect.static(require('path').resolve(dir));
-//};
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
 
 module.exports = function (grunt) {
 
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
     dist: 'dist'
   };
 
-  //var modRewrite = require('connect-modrewrite');
+  var modRewrite = require('connect-modrewrite');
 
   // Define the configuration for all the tasks
   grunt.initConfig({
@@ -81,12 +81,12 @@ module.exports = function (grunt) {
           open: true,
           middleware: function (connect) {
             return [
-              //lrSnippet,
-              //modRewrite([
-              //  '!\\.html|\\.js|\\.css|\\woff|\\ttf|\\swf$ /index.html'
-              //]),
-              //mountFolder(connect, '.tmp'),
-              //mountFolder(connect, appConfig.app),
+              lrSnippet,
+              modRewrite([
+                '!\\.html|\\.png|\\.jpg|\\.gif|\\.js|\\.css|\\woff|\\ttf|\\swf$ /index.html'
+              ]),
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, appConfig.app),
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
